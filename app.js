014ba97577ece5581e292b0b538be261c5f73d32
Craig <Craig@Craigs-MacBook-Pro-2.local>
2016-06-08 11:19:35 -0500

    fifth commit

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require ("express");
 var app = express ();
 
 var server = app.listen (process.env.PORT, function(){
-  console.log("Hello World! Good morning, good afternoon, good evening, and good night from the hostess with the mostess");
+  console.log("Good morning, good afternoon, good evening, and good night from the hostess with the mostess");
 });
 
 app.get("/",function( req , res ){
   res.writeHead(200);
+  res.write("Hello World");
   res.end();
 });
