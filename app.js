bca33d2b77d8a69e331ecff87255d64c59c156df
Craig <Craig@Craigs-MacBook-Pro-2.local>
2016-06-08 11:16:47 -0500

    fourth commit

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ var app = express ();
 var server = app.listen (process.env.PORT, function(){
   console.log("Hello World! Good morning, good afternoon, good evening, and good night from the hostess with the mostess");
 });
+
+app.get("/",function( req , res ){
+  res.writeHead(200);
+  res.end();
+});
